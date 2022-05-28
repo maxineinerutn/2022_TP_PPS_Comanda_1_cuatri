9ba21435453b6fc94d3da66e398eaf5ea6a740c0
trabajo\damoreno <damoreno@trabajo.gob.ar>
2022-05-28 12:39:52 -0300

    primer commit

---
+++ LoQueSeTeAntoje/src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  firebaseConfig: {
+  apiKey: "AIzaSyBScvj9r8EBlCFHcyoh1V48CLfs9SK5JKo",
+  authDomain: "loqueseteantojeapp.firebaseapp.com",
+  projectId: "loqueseteantojeapp",
+  storageBucket: "loqueseteantojeapp.appspot.com",
+  messagingSenderId: "474222220826",
+  appId: "1:474222220826:web:2fb951ba03b05aa204e3a3",
+  measurementId: "G-W9CF2M9TY5"
+  },
+  production:true
+}
+
+
+
