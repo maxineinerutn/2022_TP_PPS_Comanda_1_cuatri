da640ae3f3e15fcbd63af72d0683f28f894ee271
Gonzalo Sinnott Segura <gonzalo.sinnott@gmail.com>
2022-05-29 23:14:30 -0300

    Form Alta Dueño - Supervisor

--- losCincoTenedores-app/components/userRegistration/adminRegistration/AdminRegistrationScreen.tsx
+++ losCincoTenedores-app/components/userRegistration/adminRegistration/AdminRegistrationScreen.tsx
@@ -1,19 +1,209 @@
-import React, { useLayoutEffect } from "react";
-import styles from "../adminRegistration/StyleAdminRegistrationScreen";
-import { ImageBackground, TouchableOpacity, View, Image, Text } from "react-native";
-import { returnIcon, backgroundImage,  } from "../adminRegistration/AssetsAdminRegistrationScreen";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import React, { useLayoutEffect, useEffect, useState  } from "react";
+import styles from "../adminRegistration/StyleAdminRegistrationScreen";
+import { Image, ImageBackground, Text, TextInput, TouchableOpacity, View, StyleSheet } from "react-native";
+import { returnIcon, backgroundImage, cameraIcon, qrIcon,  } from "../adminRegistration/AssetsAdminRegistrationScreen";
+import Modal from "react-native-modal";
+
+import { Camera } from 'expo-camera';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { useForm } from 'react-hook-form';
+import Toast from 'react-native-simple-toast';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { ref, uploadBytes } from 'firebase/storage';
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db, storage } from "../../../App";
+import { getBlob } from "../../../utils/utils";
+import * as ImagePicker from "expo-image-picker";
+import { RadioButton } from 'react-native-paper';
+import RotatingLogo from "../../rotatingLogo/RotatingLogo";
+
+type NewUser = {
+  apellido:string;
+  nombre:string;
+  dni:string;
+  cuil:string;
+  email:string;
+  password:string;
+  confirmPassword:string
+  rol:string;
+}
 
 const AdminRegistration = () => {
 
     //CONSTANTES
     const navigation = useNavigation<NativeStackNavigationProp<any>>();
+    const [apellidoForm, setApellido] = useState("Apellido");
+    const [nombreForm, setNombre] = useState("Nombre");
+    const [dniForm, setDni] = useState("DNI");
+    const [cuilForm, setCuil] = useState("CUIL");
+    const [emailForm, setEmail] = useState("Correro Electrónico");
+    const [passwordForm, setPassword] = useState("Contraseña");
+    const [confirmPasswordForm, setConfirmPassword] = useState("Confirmar Contraseña");
+    const [scanned, setScanned] = useState(false);
+    const [openQR, setOpenQR] = useState(false);
+    const {getValues, formState:{}, reset, setValue} = useForm<NewUser>();
+    const [image, setImage] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [placeholderColor, setPlaceholderColor] = useState("white");
+    const [isModalSpinnerVisible, setModalSpinnerVisible] = useState(false);
+    const [checked, setChecked] = React.useState('DUEÑO');
 
     //RETURN
     const handleReturn = () => {
       navigation.replace("ControlPanelPropietario")
     }
+
+    //PERMISOS CAMARA
+    useEffect(() => {
+      (async () => {
+          await Camera.requestCameraPermissionsAsync();
+          await BarCodeScanner.requestPermissionsAsync();
+      })();
+    }, [])
+
+    //COMPLETADO DEL FORM A PARTIR DEL QR
+    const handleBarCodeScanned = ({ data }) => {
+      setScanned(true);
+      setOpenQR(false);
+      const dataSplit = data.split('@');
+      const dni = dataSplit[4].trim();
+      const nombre = dataSplit[2].trim();
+      const apellido = dataSplit[1].trim();
+      setValue("dni",dni);
+      setValue("nombre",nombre);
+      setValue("apellido",apellido);
+      setApellido(apellido);
+      setNombre(nombre);
+      setDni(dni);
+      setPlaceholderColor("black");
+    };
+  
+    //MANEJADOR DEL QR Y CAMARA
+    const handleOpenQR = () => {
+      setScanned(false);
+      setOpenQR(true);
+    }
+
+    const handleCamera = async (type) => {
+      let result = await ImagePicker.launchCameraAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          aspect: [4, 3],
+          quality: 1,
+      });
+      if (!result.cancelled) {
+        setImage(result["uri"]);
+      }
+    };
+
+    //SUBMIT DEL FORM
+    const onSubmit = async () => {
+      const values=getValues();
+      console.log(values);
+      let error=false;
+      Object.values(values).map(value=>{
+        if(!value){
+          error=true;
+          return;
+        }
+      })
+      if(error){
+        Toast.showWithGravity(
+          "Todos los campos son requeridos",
+          Toast.LONG, 
+          Toast.CENTER);
+        return;
+      }
+      if(!image){
+        Toast.showWithGravity(
+          "Debe tomar una foto",
+          Toast.LONG, 
+          Toast.CENTER);
+        return;
+      }
+      if(values.password!==values.confirmPassword){
+        Toast.showWithGravity(
+          "Las contraseñas no coinciden",
+          Toast.LONG, 
+          Toast.CENTER);
+        return;
+      }
+      setLoading(true)
+      toggleSpinnerAlert();
+      try {
+        //CREACION DE USUARIO
+        await createUserWithEmailAndPassword(auth,values.email,values.email);
+        console.log(auth.currentUser?.email);
+        auth.signOut();
+
+        //FORMA CROTA DE VOLVER AL USUARIO ORIGINAL
+        await signInWithEmailAndPassword(auth,"cincotenedorespropietario@gmail.com","administrador123");
+        console.log(auth.currentUser?.email);
+        
+        //UPLOAD IMAGEN
+        const blob:any = await getBlob(image);
+        const fileName = image.substring(image.lastIndexOf("/") + 1);
+        const fileRef = ref(storage, "infoDueñoSupervisor/" + fileName);
+        await uploadBytes(fileRef, blob);
+        //UPLOAD DATA
+        await addDoc(collection(db, "infoDueñoSupervisor"), {
+          lastName:values.apellido,
+          name:values.nombre,
+          dni:values.dni,
+          cuil:values.cuil,
+          email:values.email,
+          rol:checked,
+          image:fileRef.fullPath,
+          creationDate:new Date()          
+        });
+        
+        Toast.showWithGravity(
+          "Usuario creado exitosamente",
+          Toast.LONG, 
+          Toast.CENTER);      
+      reset();
+      setImage("");
+      navigation.replace('ControlPanelPropietario');
+      } catch (error:any) {
+        Toast.showWithGravity(
+          error.code,
+          Toast.LONG, 
+          Toast.CENTER); 
+      }finally{
+        setLoading(false);
+        resetForm();
+      }
+    }
+
+    //RESET DEL FORM
+    const resetForm = () => {
+      setPlaceholderColor("grey");
+      setApellido("Apellido");
+      setNombre("Nombre");
+      setDni("DNI");
+      setCuil("CUIL");
+      setEmail('Correo Electrónico');
+      setPassword('Contraseña');
+      setConfirmPassword('Confirmar Contraseña');
+      setValue("dni",'');
+      setValue("nombre",'');
+      setValue("apellido",'');
+      setValue("email",'');
+      setValue("password",'');
+      setValue("confirmPassword",'');
+      setValue("cuil",'');
+      setValue("rol",'');
+      setImage("");
+    }
+
+    //SPINNER
+    const toggleSpinnerAlert = () => {
+      setModalSpinnerVisible(true);
+      setTimeout(() => {
+        setModalSpinnerVisible(false);
+      }, 1500);
+    };
 
     //HEADER
     useLayoutEffect(() => {
@@ -34,18 +224,155 @@ const AdminRegistration = () => {
         });
       }, []);
 
-    
+    //MANEJADORES RADIOBUTTONS
+
+    const pressDueño = () => {
+      setChecked('DUEÑO');
+      setValue("rol",checked);
+      console.log(checked);
+    }
+
+    const pressSupervisor = () => {
+      setChecked('SUPERVISOR');
+      setValue("rol",checked);
+      console.log(checked);
+    }
+
     return (
-        <View style={styles.container}>
-            <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.backgroundImage} imageStyle = {{opacity:0.5}}>
-                <View style={styles.body}>
+      !openQR ?
+      <View style={styles.container}>
+          <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.backgroundImage} imageStyle = {{opacity:0.5}}>
+          {loading}
+          <View style={styles.body}>
+            <View style={styles.cameraQrContainer}>
+              {!image?
+                <TouchableOpacity onPress={handleCamera}>
+                  <Image style={styles.cameraIcon} resizeMode="cover" source={cameraIcon} />
+                </TouchableOpacity>:
+                <View>
+                  <Image style={styles.cameraImage} resizeMode="cover" source={{uri:image}}/>
+                </View>
+              }
 
-                    <Text>ALTA DUEÑO / SUPERVISOR</Text>
+              <TouchableOpacity onPress={handleOpenQR}>
+                <Image 
+                    style={styles.qrIcon} resizeMode="cover" source={qrIcon}
+                />
+              </TouchableOpacity>
+            </View>
 
-                
-                </View>                
-            </ImageBackground>           
-        </View>
+            <View style={styles.inputContainer}>
+              <View style={styles.inputField}>
+                <TextInput
+                  placeholder= {apellidoForm}
+                  placeholderTextColor= {placeholderColor}
+                  style={styles.inputText}
+                  onChangeText={(text) => setValue("apellido",text)}
+                />
+              </View>
+
+              <View style={styles.inputField}>
+                <TextInput
+                  placeholder={nombreForm}
+                  placeholderTextColor= {placeholderColor}
+                  style={styles.inputText}
+                  onChangeText={(text) => setValue("nombre",text)}
+                />
+              </View>
+
+              <View style={styles.inputField}>
+                <TextInput
+                  placeholder={dniForm}
+                  placeholderTextColor= {placeholderColor}
+                  style={styles.inputText}
+                  keyboardType={'numeric'}
+                  onChangeText={(text) => setValue("dni",text)}
+                />
+              </View>
+
+              <View style={styles.inputField}>
+                <TextInput
+                  placeholder={cuilForm}
+                  placeholderTextColor="white"
+                  style={styles.inputText}
+                  keyboardType={'numeric'}
+                  onChangeText={(text) => setValue("cuil",text)}
+                />
+              </View>
+
+              <View style={styles.inputField}>
+                <TextInput
+                  placeholder= {emailForm}
+                  placeholderTextColor="white"
+                  style={styles.inputText}
+                  onChangeText={(text) => setValue("email",text)}
+                />
+              </View>
+
+              <View style={styles.inputField}>
+                <TextInput
+                  placeholder= {passwordForm}
+                  placeholderTextColor="white"
+                  style={styles.inputText}
+                  onChangeText={(text) => setValue("password",text)}
+                  secureTextEntry = {true}
+                />
+              </View>
+
+              <View style={styles.inputField}>
+                <TextInput
+                  placeholder= {confirmPasswordForm}
+                  placeholderTextColor="white"
+                  style={styles.inputText}
+                  onChangeText={(text) => setValue("confirmPassword",text)}
+                  secureTextEntry = {true}
+                />
+              </View>
+
+              <View style={styles.inputField}>
+                <Text style={styles.tagText}>TIPO DE ALTA</Text> 
+              </View>
+
+
+              <View style={styles.inputFieldRadioLayout}>
+                <View style={styles.inputFieldRadio}>
+                  <RadioButton
+                    value="DUEÑO"
+                    status={ checked === 'DUEÑO' ? 'checked' : 'unchecked' }
+                    onPress={ pressDueño }
+                  />
+                  <Text style={styles.inputText}>DUEÑO</Text>               
+                </View>
+
+                <View style={styles.inputFieldRadio}>
+                  <RadioButton
+                    value="SUPERVISOR"
+                    status={ checked === 'SUPERVISOR' ? 'checked' : 'unchecked' }
+                    onPress={ pressSupervisor }
+                  />
+                  <Text style={styles.inputText}>SUPERVISOR</Text>
+                </View>
+              </View>
+
+
+              <View style={styles.submitContainer}>
+                <TouchableOpacity onPress={onSubmit} style={styles.buttonLayout}>
+                  <Text style={styles.buttonText}>CARGAR DUEÑO / SUPERVISOR</Text>         
+                </TouchableOpacity>
+              </View>
+            </View>          
+          </View> 
+
+          <View>
+            <Modal backdropOpacity={0} animationIn="rotate" animationOut="rotate" isVisible={isModalSpinnerVisible}>
+              <RotatingLogo></RotatingLogo>
+            </Modal>
+          </View> 
+
+          </ImageBackground>           
+      </View> : <BarCodeScanner
+                  onBarCodeScanned={scanned && openQR ? undefined : handleBarCodeScanned}
+                  style={StyleSheet.absoluteFillObject} />
     );
 };
 
