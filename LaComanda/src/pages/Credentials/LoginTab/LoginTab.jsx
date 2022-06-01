ec9b542e9eee9454da5bd7959a3a65d14d3bc825
Guido Clas <guido-clas@hotmail.com>
2022-06-01 00:08:46 -0300

    SplashScreen - Credentials tabs - Login/Register

---
+++ LaComanda/src/pages/Credentials/LoginTab/LoginTab.jsx
@@ -0,0 +1,23 @@
+import {
+  KeyboardAvoidingView,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  ActivityIndicator,
+  ImageBackground
+} from "react-native";
+import React, { useContext, useState } from "react";
+import { styles } from "./styles";
+
+const LoginTab = () => {
+  
+  return (
+    <KeyboardAvoidingView style={{}}>
+
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginTab;
---
+++ LaComanda/src/pages/Credentials/LoginTab/utils.js
@@ -0,0 +1,21 @@
+const handleLoginErrorMessage = (code) => {
+    switch (code) {
+      case 'auth/user-disabled': {
+          return 'Usuario deshabilitado';
+      }
+      case 'auth/invalid-email': {
+          return 'Email inválido';
+      }
+      case 'auth/user-not-found': {
+        return 'No se encontró un usuario con éste mail';
+      }
+      case 'auth/wrong-password': {
+        return 'Contraseña incorrecta';
+      }
+      default: {
+          return 'Contraseña incorrecta';
+      }
+    }
+};
+
+export default handleLoginErrorMessage;
