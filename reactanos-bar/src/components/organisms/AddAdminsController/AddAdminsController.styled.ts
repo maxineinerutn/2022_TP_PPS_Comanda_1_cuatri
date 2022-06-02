ae83938e8ab4e70de10bf85bc6789fc6bcadf23f
lucianorojas <Bienvenido3354>
2022-06-02 00:35:44 -0300

    Agregada pantalla de admin-supervisor

---
+++ reactanos-bar/src/components/organisms/AddAdminsController/AddAdminsController.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const StyledView = styled.View`
+    width:100%;
+`
+
+export const StyledMargin = styled.View`
+    margin-vertical:15px;
+`
