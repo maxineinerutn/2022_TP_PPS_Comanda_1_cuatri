46e238ee5c0fb14fa7bcabfc844ff29e00f64411
Gonzalo Sinnott Segura <gonzalo.sinnott@gmail.com>
2022-06-02 20:26:49 -0300

    fix altaMesa

--- losCincoTenedores-app/components/userRegistration/tableRegistration/TableRegistrationScreen.tsx
+++ losCincoTenedores-app/components/userRegistration/tableRegistration/TableRegistrationScreen.tsx
@@ -1,18 +1,16 @@
 import React, { useLayoutEffect, useEffect, useState, useCallback  } from "react";
 import styles from "../tableRegistration/StyleTableRegistrationScreen";
 import { ImageBackground, TouchableOpacity, View, Image, Text, TextInput, StyleSheet } from "react-native";
-import { returnIcon, backgroundImage, cameraIcon, qrIcon,  } from "../tableRegistration/AssetsTableRegistrationScreen";
+import { returnIcon, backgroundImage, cameraIcon } from "../tableRegistration/AssetsTableRegistrationScreen";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import Modal from "react-native-modal";
-
 import { Camera } from 'expo-camera';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { useForm } from 'react-hook-form';
 import Toast from 'react-native-simple-toast';
-import { createUserWithEmailAndPassword } from "firebase/auth";
 import { ref, uploadBytes } from 'firebase/storage';
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import { auth, db, storage } from "../../../App";
 import { getBlob } from "../../../utils/utils";
 import * as ImagePicker from "expo-image-picker";
@@ -20,14 +18,6 @@ import { RadioButton } from 'react-native-paper';
 import RotatingLogo from "../../rotatingLogo/RotatingLogo";
 
 type NewUser = {
-  // apellido:string;
-  // nombre:string;
-  // dni:string;
-  // cuil:string;
-  // email:string;
-  // // password:string;
-  // // confirmPassword:string
-  // rol:string;
   number:string;
   capacity:string;
   type:string;
@@ -37,66 +27,29 @@ const TableRegistration = () => {
 
     //CONSTANTES
     const navigation = useNavigation<NativeStackNavigationProp<any>>();
-    // const [apellidoForm, setApellido] = useState("Apellido");
-    // const [nombreForm, setNombre] = useState("Nombre");
     const [numberForm, setNumber] = useState("Numero de Mesa");
     const [capacityForm, setCapacity] = useState("Cantidad de Comensales");
-    // const [cuilForm, setCuil] = useState("CUIL");
-    // const [emailForm, setEmail] = useState("Correro Electrónico");
-    // const [passwordForm, setPassword] = useState("Contraseña");
-    // const [confirmPasswordForm, setConfirmPassword] = useState("Confirmar Contraseña");
-    const [scanned, setScanned] = useState(false);
-    const [openQR, setOpenQR] = useState(false);
     const {getValues, formState:{}, reset, setValue} = useForm<NewUser>();
     const [image, setImage] = useState("");
     const [loading, setLoading] = useState(false);
     const [placeholderColor, setPlaceholderColor] = useState("white");
     const [isModalSpinnerVisible, setModalSpinnerVisible] = useState(false);
-    const [checked, setChecked] = React.useState('Dueño');
-
-
-        //VARIABLE PARA GUARDAR EL USUARIO ORIGINAL
-        let originalUser = auth.currentUser;
+    const [checked, setChecked] = React.useState('Estandar');
 
     //RETURN
     const handleReturn = () => {
       navigation.replace("ControlPanelPropietario")
     }
 
-        //PERMISOS CAMARA
-        useEffect(() => {
-          (async () => {
-              await Camera.requestCameraPermissionsAsync();
-              await BarCodeScanner.requestPermissionsAsync();
-          })();
-        }, [])
+    //PERMISOS CAMARA
+    useEffect(() => {
+      (async () => {
+          await Camera.requestCameraPermissionsAsync();
+          await BarCodeScanner.requestPermissionsAsync();
+      })();
+    }, [])      
 
-      //COMPLETADO DEL FORM A PARTIR DEL QR
-      const handleBarCodeScanned = ({ data }) => {
-      setScanned(true);
-      setOpenQR(false);
-      const dataSplit = data.split('@');
-      const capacity = dataSplit[2].trim();
-      // const nombre = dataSplit[2].trim();
-      // const apellido = dataSplit[1].trim();
-      const number = dataSplit[1].trim();
-      setValue("capacity",capacity);
-      // setValue("nombre",nombre);
-      // setValue("apellido",apellido);
-      setValue("number",number);
-      // setApellido(apellido);
-      // setNombre(nombre);
-      setCapacity(capacity);
-      setNumber(number);
-      setPlaceholderColor("black");
-    };
-
-    //MANEJADOR DEL QR Y CAMARA
-    const handleOpenQR = () => {
-      setScanned(false);
-      setOpenQR(true);
-    }
-
+    //MANEJADOR DE LA CAMARA
     const handleCamera = async (type) => {
       let result = await ImagePicker.launchCameraAsync({
           mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -135,46 +88,37 @@ const TableRegistration = () => {
           Toast.CENTER);
         return;
       }
-      // if(values.password!==values.confirmPassword){
-      //   Toast.showWithGravity(
-      //     "Las contraseñas no coinciden",
-      //     Toast.LONG, 
-      //     Toast.CENTER);
-      //   return;
-      // }
       setLoading(true)
       toggleSpinnerAlert();
       try {
-        console.log(auth.currentUser?.email);
-        // //CREACION DE USUARIO
-        // await createUserWithEmailAndPassword(auth,values.email,values.email);
-        // console.log(auth.currentUser?.email);
 
-        // //DESLOGUEO DEL USUARIO CREADO Y REESTABLECIMIENTO DEL USUARIO ORIGINAL
-        // await auth.signOut();
-        // await auth.updateCurrentUser(originalUser);
+        //VERIFICACION SI EL NUMERO DE MESA YA EXISTE
+        const q = query(collection(db, "tableInfo"), where("tableNumber", "==", values.number));
+        const querySnapshot = await getDocs(q);
+
+        if(querySnapshot.size > 0){
+          Toast.showWithGravity(
+            "El numero de mesa ya existe",
+            Toast.LONG, 
+            Toast.CENTER);
+          toggleSpinnerAlert();
+          setLoading(false);
+          resetForm();  
+          return;        
+        } 
 
         //UPLOAD IMAGEN
         const blob:any = await getBlob(image);
         const fileName = image.substring(image.lastIndexOf("/") + 1);
         const fileRef = ref(storage, "tableInfo/" + fileName);
         await uploadBytes(fileRef, blob);
+        
         //UPLOAD DATA
         await addDoc(collection(db, "tableInfo"), {
-          // lastName:values.apellido,
-          // name:values.nombre,
-          // dni:values.dni, 
-          // mesa:values.mesa,
-          // cuil:values.cuil,
-          // email:values.email,
-          // rol:checked,
-          // image:fileRef.fullPath,
-
-          number:values.number,
-          capacity:values.capacity,
-          type:checked,
+          tableNumber:values.number,
+          tableCapacity:values.capacity,
+          tableType:checked,
           image:fileRef.fullPath,
-
           creationDate:new Date()          
         });        
         Toast.showWithGravity(
@@ -183,8 +127,7 @@ const TableRegistration = () => {
           Toast.CENTER);      
       reset();
       setImage("");
-      //VUELTA AL CONTROL PANEL ( VER DE PONER EL QUE CORRESPONDE EN CADA CASO)
-      handleReturn;
+      handleReturn();
       } catch (error:any) {
         Toast.showWithGravity(
           error.code,
@@ -193,29 +136,16 @@ const TableRegistration = () => {
       }finally{
         setLoading(false);
         resetForm();  
-        console.log(auth.currentUser?.email);
       }
     }
 
     //RESET DEL FORM
     const resetForm = () => {
       setPlaceholderColor("white");
-      // setApellido("Apellido");
-      // setNombre("Nombre");
       setCapacity("Numero de Comensales");
-      setNumber("Number");
-      // setCuil("CUIL");
-      // setEmail('Correo Electrónico');
-      // setPassword('Contraseña');
-      // setConfirmPassword('Confirmar Contraseña');
+      setNumber("Numero de Mesa");
       setValue("number","");
       setValue("capacity",'');
-      // setValue("nombre",'');
-      // setValue("apellido",'');
-      // setValue("email",'');
-      // setValue("password",'');
-      // setValue("confirmPassword",'');
-      // setValue("cuil",'');
       setValue("type",'');
       setImage("");
     }
@@ -249,58 +179,36 @@ const TableRegistration = () => {
 
           //MANEJADORES RADIOBUTTONS
 
-    const pressDueño = () => {
-      setChecked('Dueño');
+    const pressDiscapacitados = () => {
+      setChecked('Discapacitados');
     }
 
-    const pressSupervisor = () => {
-      setChecked('Supervisor');
+    const pressVip = () => {
+      setChecked('VIP');
     }
 
     const pressEstandar = () => {
       setChecked('Estandar');
     }
 
-    // const pressEtc = () => {
-    //   setChecked('Etc');
-    // }
-
-
-
     //CARGA CAMPOS SEGUN SELECCION RADIO BUTTON
     useFocusEffect(
       useCallback(() => {
         console.log(checked);
-        if(checked=='Supervisor'){
+        if(checked=='Discapacitados'){
           setValue("type",checked);
         }
-        if(checked=='Dueño'){
+        if(checked=='VIP'){
           setValue("type",checked);
         }
         if(checked=='Estandar'){
           setValue("type",checked);
         }
-        // if(checked=='Etc'){
-        //   setValue("rol",checked);
-        // }
-    }, [checked]))
-    
-//     return (
-//         <View style={styles.container}>
-//             <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.backgroundImage} imageStyle = {{opacity:0.5}}>
-//                 <View style={styles.body}>
+    }, [checked]))    
 
-//                     <Text>ALTA MESA</Text>
-                
-//                 </View>                
-//             </ImageBackground>           
-//         </View>
-//     );
-// };
 return (
-  !openQR ?
   <View style={styles.container}>
-      <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.backgroundImage} imageStyle = {{opacity:0.5}}>
+    <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.backgroundImage} imageStyle = {{opacity:0.5}}>
       {loading}
       <View style={styles.body}>
         <View style={styles.cameraQrContainer}>
@@ -311,34 +219,10 @@ return (
             <View>
               <Image style={styles.cameraImage} resizeMode="cover" source={{uri:image}}/>
             </View>
-          }
-
-          {/* <TouchableOpacity onPress={handleOpenQR}>
-            <Image 
-                style={styles.qrIcon} resizeMode="cover" source={qrIcon}
-            />
-          </TouchableOpacity> */}
+          }         
         </View>
 
         <View style={styles.inputContainer}>
-          {/* <View style={styles.inputField}>
-            <TextInput
-              placeholder= {apellidoForm}
-              placeholderTextColor= {placeholderColor}
-              style={styles.inputText}
-              onChangeText={(text) => setValue("apellido",text)}
-            />
-          </View> */}
-
-          {/* <View style={styles.inputField}>
-            <TextInput
-              placeholder={nombreForm}
-              placeholderTextColor= {placeholderColor}
-              style={styles.inputText}
-              onChangeText={(text) => setValue("nombre",text)}
-            />
-          </View> */}
-
           <View style={styles.inputField}>
             <TextInput
               placeholder={numberForm}
@@ -357,70 +241,13 @@ return (
               keyboardType={'numeric'}
               onChangeText={(text) => setValue("capacity",text)}
             />
-          </View>
-
-          {/* <View style={styles.inputField}>
-            <TextInput
-              placeholder={cuilForm}
-              placeholderTextColor="white"
-              style={styles.inputText}
-              keyboardType={'numeric'}
-              onChangeText={(text) => setValue("cuil",text)}
-            />
-          </View> */}
-
-          {/* <View style={styles.inputField}>
-            <TextInput
-              placeholder= {emailForm}
-              placeholderTextColor="white"
-              style={styles.inputText}
-              onChangeText={(text) => setValue("email",text)}
-            />
-          </View> */}
-
-          {/* <View style={styles.inputField}>
-            <TextInput
-              placeholder= {passwordForm}
-              placeholderTextColor="white"
-              style={styles.inputText}
-              onChangeText={(text) => setValue("password",text)}
-              secureTextEntry = {true}
-            />
-          </View> */}
-
-          {/* <View style={styles.inputField}>
-            <TextInput
-              placeholder= {confirmPasswordForm}
-              placeholderTextColor="white"
-              style={styles.inputText}
-              onChangeText={(text) => setValue("confirmPassword",text)}
-              secureTextEntry = {true}
-            />
-          </View> */}
+          </View>          
 
           <View style={styles.inputField}>
             <Text style={styles.tagText}>TIPO</Text> 
           </View>
 
-
           <View style={styles.inputFieldRadioLayout}>
-            <View style={styles.inputFieldRadio}>
-              <RadioButton
-                value="Dueño"
-                status={ checked === 'Dueño' ? 'checked' : 'unchecked' }
-                onPress={ pressDueño }
-              />
-              <Text style={styles.inputText}>VIP</Text>               
-            </View>
-
-            <View style={styles.inputFieldRadio}>
-              <RadioButton
-                value="Supervisor"
-                status={ checked === 'Supervisor' ? 'checked' : 'unchecked' }
-                onPress={ pressSupervisor }
-              />
-              <Text style={styles.inputText}>DISCAPACITADOS</Text>
-            </View>
 
             <View style={styles.inputFieldRadio}>
               <RadioButton
@@ -431,16 +258,26 @@ return (
               <Text style={styles.inputText}>ESTÁNDAR</Text>
             </View>
 
-            {/* <View style={styles.inputFieldRadio}>
+            <View style={styles.inputFieldRadio}>
               <RadioButton
-                value="Etc"
-                status={ checked === 'Etc' ? 'checked' : 'unchecked' }
-                onPress={ pressEtc }
+                value="VIP"
+                status={ checked === 'VIP' ? 'checked' : 'unchecked' }
+                onPress={ pressVip }
               />
-              <Text style={styles.inputText}>ETC</Text>
-            </View> */}
-          </View>
+              <Text style={styles.inputText}>VIP</Text>               
+            </View>
 
+            <View style={styles.inputFieldRadio}>
+              <RadioButton
+                value="Discapacitados"
+                status={ checked === 'Discapacitados' ? 'checked' : 'unchecked' }
+                onPress={ pressDiscapacitados }
+              />
+              <Text style={styles.inputText}>DISCAPACITADOS</Text>
+            </View>
+
+                        
+          </View>
 
           <View style={styles.submitContainer}>
             <TouchableOpacity onPress={onSubmit} style={styles.buttonLayout}>
@@ -456,11 +293,8 @@ return (
         </Modal>
       </View> 
 
-      </ImageBackground>           
-  </View> : <BarCodeScanner
-              onBarCodeScanned={scanned && openQR ? undefined : handleBarCodeScanned}
-              style={StyleSheet.absoluteFillObject} />
-);
+    </ImageBackground>           
+  </View>);
 };
 
 export default TableRegistration;
