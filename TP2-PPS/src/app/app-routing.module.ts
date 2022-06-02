1a48115027bcc6b0941efa1492311a4a9fba25c6
Sagnella Franco <Fransagn@gmail.com>
2022-06-02 12:06:08 -0300

    auth guard (#5)

--- TP2-PPS/src/app/app-routing.module.ts
+++ TP2-PPS/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './paginas/home/home.component';
 
 const routes: Routes = [
-  {path:'home', component:HomeComponent},
+  {path:'home', component:HomeComponent, canActivate:[AuthGuard]},
   {
     path: 'usuarios',
     loadChildren: () => import('./paginas/usuarios/usuarios.module').then(m => m.UsuariosModule),
