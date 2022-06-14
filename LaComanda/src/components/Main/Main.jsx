47870307f076db93138338eeb3bca32ecaa0350f
Guido Clas <guido-clas@hotmail.com>
2022-06-14 00:54:11 -0300

    added config for root navigation and iconheader

--- LaComanda/src/components/Main/Main.jsx
+++ LaComanda/src/components/Main/Main.jsx
@@ -1,22 +1,40 @@
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import { React } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Credentials from '../../pages/Credentials/Credentials';
 import Home from '../../pages/Home/Home';
 import theme from '../../config/theme';
 import Approvals from '../../pages/Approvals/Approvals';
 import Addition from '../../pages/Addition/Addition';
+import { signOutUser } from '../../services/AuthService';
+import { navigationRef, navigate } from '../../config/RootNavigation';
 
 const Stack = createNativeStackNavigator();
 
 export default function Main() {
-  // const { isLogged } = useContext(GlobalContext);
+  const handleLogout = () => {
+    signOutUser();
+    navigate( 'Credentials', { screen: 'Ingresar' });
+  };
+
+  function renderHeaderLogoutIcon() {
+    return (
+      <TouchableOpacity onPress={handleLogout}>
+        <MaterialCommunityIcons name='logout' size={40} color='white' />
+      </TouchableOpacity>
+    );
+  }
 
   return (
     <View style={{ flex: 1 }}>
-      <NavigationContainer>
-        <Stack.Navigator>
+      <NavigationContainer ref={navigationRef}>
+        <Stack.Navigator
+          screenOptions={{
+            headerRight: () => renderHeaderLogoutIcon()
+          }}
+        >
           <Stack.Screen
             options={{
               title: 'Aplicación',
